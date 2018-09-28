7435d2bba67fdf82856ba963fab586c01e3cf3d2
Rodrigo Dias <diasbass@hotmail.com>
2018-09-28 16:33:03 -0300

    Update wax-on-wax-off-js

--- remove-event-listener.js
+++ remove-event-listener.js
@@ -1,6 +1,6 @@
-var elemento = document.getElementById('search-field');
+var element = document.getElementById('myElementId');
 var listener = function (event) {
   console.log('clicked!')
 };
-elemento.addEventListener('click', listener, false);
-elemento.removeEventListener('click', listener, false); // Like a off() jQuery
+element.addEventListener('click', listener, false);
+element.removeEventListener('click', listener, false); // Like a off() jQuery
